860f77bab08965b98dba09f6de020c6ee5a4e92d
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-07-16 19:46:09 +0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main_Page />} />
-          <Route path="animation" element={<AnimatinTest />} />
-          <Route path="animationTest" element={<AnimationTestNew />} />
+          <Route index element={<AnimatinTest />} />
+          {/* <Route path="animation" element={<AnimatinTest />} />
+          <Route path="animationTest" element={<AnimationTestNew />} /> */}
           <Route path="*" element={<Non_Found_Page />} />
         </Route>
       </Routes>
