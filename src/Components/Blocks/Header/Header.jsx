14c96d0f68c4ca14eaa249d07af51d131a6063d0
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-07-31 11:47:56 +0300

    Update

--- src/Components/Blocks/Header/Header.jsx
+++ src/Components/Blocks/Header/Header.jsx
@@ -4,9 +4,9 @@ import classes from './Header.module.css';
 function Header({ children, ...props }) {
     return ( 
         <header className={classes.header}>
-            <a href="tel:+70000000000" style={{fontFamily: 'Gotham 400'}}>+7 (000) 000 00 00</a>
+            <a href="tel:+79380357788" style={{fontFamily: 'Gotham 400'}}>+7 (938) 035 77 88</a>
             <img src="/logo.png" alt="" />
-            <a href="">Контакты</a>
+            <a href="/">Контакты</a>
         </header>
      );
 }
