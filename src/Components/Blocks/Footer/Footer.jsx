6b6db1df98f748352af78aea45706f455f20d3dc
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2026-01-13 14:57:30 +0300

    upd add link

--- src/Components/Blocks/Footer/Footer.jsx
+++ src/Components/Blocks/Footer/Footer.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import classes from './Footer.module.css';
 
 function Footer({ children, ...props }) {
-    return ( 
+    return (
         <footer className={classes.footer}>
-            © КД Времена года 2025
+            <div className={classes.footer_mob}></div>
+            <div>
+                © КД Времена года 2026
+            </div>
+
+            <div>
+                {/* <a href="https://alazarstudio.ru" target="_blank" rel="noopener noreferrer">
+                    <img src="/logo.png" alt="" />
+                </a> */}
+                Разработка сайта: <a href="https://alazarstudio.ru" target="_blank" rel="noopener noreferrer">Alazar Studio</a> 
+            </div>
         </footer>
-     );
+    );
 }
 
 export default Footer;
